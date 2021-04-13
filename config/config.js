c8fe5488d18f52fcfe6d791c71ad4f0ec10cc839
maria-luiza-duda <marialuizaduda00@gmail.com>
2021-04-13 14:14:41 -0300

    Projeto instagram completo

---
+++ config/config.js
@@ -0,0 +1,11 @@
+[
+  {
+    "development": {
+      "username": "root",
+      "password": 230700,
+      "database": "instagram",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    }
+  }
+]
